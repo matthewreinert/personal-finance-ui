c37c377630bf2323149cf59b358d9d83eff0b5de
Matt Reinert <matt.reinert@gmail.com>
2023-11-21 09:17:15 -0500

    fixing tests now, coverage later

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { WebAuthnService } from '../web-authn.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
+
+    const webAuthnService = jasmine.createSpyObj('WebAuthnService', ['loginUser']);
+
     await TestBed.configureTestingModule({
+      providers: [{ provide: WebAuthnService, useValue: webAuthnService }],
       imports: [LoginComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { of } from 'rxjs';
+import { WebAuthnService } from '../web-authn.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
+
+    const webAuthnService = jasmine.createSpyObj('WebAuthnService', ['registerUser']);
+
     await TestBed.configureTestingModule({
+      providers: [{ provide: WebAuthnService, useValue: webAuthnService }],
       imports: [RegisterComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
